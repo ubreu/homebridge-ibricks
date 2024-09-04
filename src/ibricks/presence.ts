a1b63ce98843fd90efb693a319570767eff918c4
Urs Breu <urs.breu@ergon.ch>
2024-09-04 16:47:23 +0200

    fix mocha-based tests

--- src/ibricks/presence.ts
+++ src/ibricks/presence.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import cheerio from 'cheerio';
+import * as cheerio from 'cheerio';
 import { IBricksServer } from './server';
 
 export enum Presence {
--- src/ibricks/server.spec.ts
+++ src/ibricks/server.spec.ts
@@ -3,8 +3,8 @@ import { IBricksServer, getServer } from './server';
 
 const serverUrl= 'http://192.168.3.10/';
 
-describe('Presence Tests', () => {
-  it('should flip the presence status', async () => {
+describe('Server Test', () => {
+  it('get server details', async () => {
     const server = await getServer(serverUrl);
     assert.equal(server.url, serverUrl);
     assert.equal(server.name, 'UMO iBricks');
